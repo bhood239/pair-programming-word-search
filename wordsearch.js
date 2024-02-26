c407014d195c2abd451c51cfc6f022501217ed30
Ben Hood <benhood97@btinternet.com>
2024-02-25 18:34:03 -0800

    refactor

--- wordsearch.js
+++ wordsearch.js
@@ -2,47 +2,47 @@
 
 const wordSearch = (letters, word) => {
   const horizontalJoin = letters.map(ls => ls.join(''));
-      for (const l of horizontalJoin) {
-          if (l.includes(word)) {
-            return true;
-          } 
-      }
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
     
-    const transposedLetters = transpose(letters);
-    const verticalJoin = transposedLetters.map(ls => ls.join(''));
-    for (const l of verticalJoin) {
-        if (l.includes(word)) {
-          return true;
-        } 
+  const transposedLetters = transpose(letters);
+  const verticalJoin = transposedLetters.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-
-    for (let l = 0; l < horizontalJoin.length; l++) {
-        let rl = horizontalJoin[l].split('').reverse().join('');
-        if (rl.includes(word)) {
-          return true;
-        } 
-    }
-
-    for (let l = 0; l < verticalJoin.length; l++) {
-      let rl = verticalJoin[l].split('').reverse().join('');
-      if (rl.includes(word)) {
-        return true;
-      } 
   }
 
-    return false;
-}
-
-const transpose = function(matrix) {
-    let result = [];
-    for (let j = 0; j < matrix[0].length; j++) {
-        let newRow = [];
-        for (let i = 0; i < matrix.length; i++) {
-            newRow.push(matrix[i][j]);
-        }
-        result.push(newRow);
+  for (let l = 0; l < horizontalJoin.length; l++) {
+    let rl = horizontalJoin[l].split('').reverse().join('');
+    if (rl.includes(word)) {
+      return true;
     }
-    return result;
+  }
+
+  for (let l = 0; l < verticalJoin.length; l++) {
+    let rl = verticalJoin[l].split('').reverse().join('');
+    if (rl.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let result = [];
+  for (let j = 0; j < matrix[0].length; j++) {
+    let newRow = [];
+    for (let i = 0; i < matrix.length; i++) {
+      newRow.push(matrix[i][j]);
+    }
+    result.push(newRow);
+  }
+  return result;
+};
+
+module.exports = wordSearch;
